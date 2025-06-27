4174984faa7e8189d06d299dd03c307bab40dc71
romann <romann.belmer@gmail.com>
2025-06-27 15:20:17 +0200

    fix on on warning

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,8 @@ use sdl2::pixels::{Color};
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::rect::{FPoint, Point, Rect};
-use sdl2::render::{Canvas, TextureCreator};
-use sdl2::video::{Window, WindowContext};
+use sdl2::render::{Canvas};
+use sdl2::video::{Window};
 use sdl2::EventPump;
 use sdl2::ttf::{self, Font};
 
