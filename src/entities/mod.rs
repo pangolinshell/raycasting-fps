1f9696f9769485badfa6b50016bcf532ee89c612
romann <romann.belmer@gmail.com>
2025-06-28 14:52:08 +0200

    name changed

--- src/entities/mod.rs
+++ src/entities/mod.rs
@@ -2,10 +2,10 @@ mod player;
 mod entity;
 mod render_data;
 mod unliving;
-mod path_follower;
+mod pather;
 
 pub use entity::{Entity,Context, PlacementData};
 pub use render_data::RenderData;
 pub use player::Player;
 pub use unliving::NotMoving;
-pub use path_follower::Straffer;
+pub use pather::Straffer;
